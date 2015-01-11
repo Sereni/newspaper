8bb7ae160bac39f3dde6709a604ae947e53815ab
Sereni <sereni.nm@gmail.com>
2015-01-11 14:09:51 +0300

    Taught the test corpus script to decide which urls to parse

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -17,7 +17,10 @@ LOG_LEVEL = 'INFO'
 COOKIES_ENABLED = False
 
 # this will result in approx 50 pages, since crawls are async and do not stop immediately
-CLOSESPIDER_PAGECOUNT = 42
+# CLOSESPIDER_PAGECOUNT = 42
+
+# this setting is for quick tests
+CLOSESPIDER_PAGECOUNT = 5
 
 # this helps against 503s on some sites
 DOWNLOAD_DELAY = 1
--- newspaper/spiders/test50.py
+++ newspaper/spiders/test50.py
@@ -9,15 +9,20 @@ from the name variable in each class.
 
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors import LinkExtractor
+from scrapy.exceptions import CloseSpider
+
+from collections import Counter
 
 import os
 import errno
+import re
 
 
 class TestSpider(CrawlSpider):
 
     dir = ''
     prefix = 'newspaper/downloads/'
+    start_urls = []
 
     def parse_link(self, response):
 
@@ -34,6 +39,76 @@ class TestSpider(CrawlSpider):
         with open(path, 'w') as f:
             f.write(response.body)
 
+    def update_rules(self, links):
+
+        # substitute all digit sequences with \d+. this is where it breaks if articles have names instead of ids.
+        patterns = set([re.sub('\d+', '\d+', link).replace('\\\\', '\\') for link in links])
+
+        # create spider rules based on patterns
+        self.rules = tuple([Rule(LinkExtractor(allow=pattern), callback='parse_link', follow=True)
+                            for pattern in patterns])
+
+        # update rules
+        super(TestSpider, self)._compile_rules()
+
+    def parse_start_url(self, response):
+
+        # get all the nodes that have any text. I did look for better approaches. suggestions welcome.
+        nodes = response.xpath('//text()/..')
+        previews = []
+
+        # filter text by length, get rid of scripts, write down
+        for node in nodes:
+            text = node.xpath('./text()').extract()[0]
+            if len(text.split()) > 15 and (not 'var' in text and not '{' in text and not '<' in text):
+                previews.append(node)
+
+        links = Counter()  # the reason it's a counter is that we'll want to dump anything too frequent later on
+
+        for node in previews:
+
+            # first, check if the node has a href associated with it -- if it does, this is the article link
+            self_link = node.xpath('./@href')
+            if self_link:
+                links[self_link.extract()[0]] += 1
+                continue
+
+            # look for previous siblings that are <a> with 2+ words
+            # allow to rise for 2 levels of depth to account for divs (if false positives, look here)
+            depth = 0
+            lift = '../'
+
+            while depth < 3:
+
+                # find the preceding <a> siblings that have text over 2 words long, write down the links
+                link_nodes = node.xpath('./{0}preceding-sibling::node()/descendant-or-self::a'.format(lift * depth))
+                new_links = [n.xpath('./@href').extract()[0] for n in link_nodes if n.xpath('./text()') and
+                             n.xpath('./text()').extract()[0].split() > 2 and
+                             n.xpath('./@href')]  # there are a lot of 2-word titles, but also a lot of trash
+
+                # will only go up a level if nothing found on this level. hopefully not that often.
+                if new_links:
+                    for link in new_links:
+                        links[link] += 1
+                    break
+                depth += 1
+
+        # now process the titles without the preview text
+        # collect all links with captions over 5 words long
+        nodes = response.xpath('//a')
+        for node in nodes:
+            if node.xpath('./text()') and len(node.xpath('./text()').extract()[0].split()) > 4:
+                url = node.xpath('./@href').extract()[0]
+                links[url] += 1
+
+        # filter
+        exp = re.compile('\d+')
+        link_list = [key for (key, value) in links.items() if value < 3  # repetitive links are usually topics
+                     and key.startswith('/')  # relative links only, no externals
+                     and exp.search(key)]  # articles often have numbers, sections don't
+
+        self.update_rules(link_list)
+
 
 class RbcSpider(TestSpider):
 
@@ -43,10 +118,6 @@ class RbcSpider(TestSpider):
         'http://rbcdaily.ru'
     ]
 
-    rules = (
-        Rule(LinkExtractor(allow='/[\w]+/[0-9]+'), callback='parse_link', follow=True),
-    )
-
 
 class SovietSpider(TestSpider):
     name = 'sovsport'
@@ -55,19 +126,11 @@ class SovietSpider(TestSpider):
         'http://sovsport.ru'
     ]
 
-    rules = (
-        Rule(LinkExtractor(allow='/[\w]+/(text|article)-item/[0-9]+'), callback='parse_link', follow=True),
-    )
-
 
 class RiaSpider(TestSpider):
     name = 'ria'
     allowed_domains = ['ria.ru']
     start_urls = ['http://ria.ru']
-
-    rules = (
-        Rule(LinkExtractor(allow='/[\w]+/[0-9]+/[0-9]+.html'), callback='parse_link', follow=True),
-    )
 
 
 class IzvestiaSpider(TestSpider):
@@ -75,16 +138,8 @@ class IzvestiaSpider(TestSpider):
     allowed_domains = ['izvestia.ru']
     start_urls = ['http://izvestia.ru']
 
-    rules = (
-        Rule(LinkExtractor(allow='/news/[0-9]+'), callback='parse_link', follow=True),
-    )
-
 
 class KpSpider(TestSpider):
     name = 'kp'
     allowed_domains = ['kp.ru']
     start_urls = ['http://kp.ru']
-
-    rules = (
-        Rule(LinkExtractor(allow='/online/news/[0-9]+'), callback='parse_link', follow=True),
-    )
